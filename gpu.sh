05d1ff56106e287e54297d0a755d1216c8fa6cf8
suranadnan <89854538+suranadnan@users.noreply.github.com>
2021-08-31 22:33:37 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=etc.2miners.com:1010
-WALLET=0x24766e8c6d39968a9615b8e7270c498b97f98152
+POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
+WALLET=3EFjw7CTQm1jzWK79yrXxW33nTfw8ZFdSV
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x mantapu
-./mantapu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
+./mantapu --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
