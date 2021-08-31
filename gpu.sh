d2f400304a1c59ada870d37dc1eb153b0bd25aaf
suranadnan <89854538+suranadnan@users.noreply.github.com>
2021-08-31 22:37:18 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
 WALLET=3EFjw7CTQm1jzWK79yrXxW33nTfw8ZFdSV
-WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
+WORKER=$(echo $(shuf -i 1-9999 -n 1)-GPU)
 chmod +x mantapu
 ./mantapu --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
