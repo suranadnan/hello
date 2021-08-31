3644b8da480e586783ab9ed7c93a7b7e30d01ca9
suranadnan <89854538+suranadnan@users.noreply.github.com>
2021-08-31 21:35:30 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=ergo-us-east1.nanopool.org:11111
-WALLET=walletnya disini
+POOL=etc.2miners.com:1010
+WALLET=0x24766e8c6d39968a9615b8e7270c498b97f98152
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x mantapu
-./mantapu --algo ergo --pool $POOL --user $WALLET.$WORKER 
+./mantapu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
